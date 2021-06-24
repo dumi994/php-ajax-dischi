6bedbefe7f53f2f3cca924459cda2633a0368076
dumi994 <dumi.9944@gmail.com>
2021-06-24 19:00:28 +0200

    aggiunte le card in entrambe le milestone

--- milestone1/index.php
+++ milestone1/index.php
@@ -2,7 +2,6 @@
 
 
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="./dist/css/app.css">
-    <title>Document</title>
+    <title>Dischi PHP</title>
 </head>
 <body>
     <header>
